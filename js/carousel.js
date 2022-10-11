150bc5c72eb323a5c3c3c8c7bcbfd01bbc95def2
Yue-plus <Yue_plus@foxmail.com>
2022-10-11 20:09:46 +0800

    使图片能点击跳转

--- js/carousel.js
+++ js/carousel.js
@@ -1,31 +1,29 @@
 let carouselList = [
-  { serial: '01', title: '第二把赤霄', desc: '#决战#' },
-  { serial: '02', title: '笔记', desc: '#企鹅物流的秘密#' },
-  { serial: '03', title: '安洁莉娜', desc: '#信使#' },
-  { serial: '04', title: '莱茵生命', desc: '#科研#' },
-  { serial: '05', title: '端午', desc: '#炎国水乡#' },
-  { serial: '06', title: '龙门', desc: '#邻街一角#' },
-  { serial: '07', title: '谜团', desc: '#源石#' },
-  { serial: '08', title: '启航', desc: '#任务外出#' },
-  { serial: '09', title: '企鹅物流', desc: '#办公室#' },
-  { serial: '10', title: '启示', desc: '#大厅#' },
-  { serial: '11', title: '死灰复燃', desc: '#不死的黑蛇#' },
-  { serial: '12', title: '苏醒', desc: '#石棺之间#' },
-  { serial: '13', title: '乌萨斯学生自治团', desc: '#回忆中的教室#' },
-  { serial: '14', title: '瑕光', desc: '#胜利的荣光#' },
-  { serial: '15', title: '谢拉格', desc: '#喀兰贸易#' },
-  { serial: '16', title: '预备组', desc: '#行动预备组A4#' },
-  { serial: '17', title: '整装出发', desc: '#印象图#' },
-  { serial: '18', title: '追忆', desc: '#过去的村庄#' },
+  { serial: '01', title: '第二把赤霄', desc: '#决战#', href: 'https://prts.wiki/w/%E9%98%BF%E7%B1%B3%E5%A8%85(%E8%BF%91%E5%8D%AB)' },
+  { serial: '02', title: '笔记', desc: '#企鹅物流的秘密#', href: 'https://prts.wiki/w/%E6%B3%B0%E6%8B%89%E5%A4%A7%E5%85%B8:%E7%BB%84%E7%BB%87/%E4%BC%81%E9%B9%85%E7%89%A9%E6%B5%81' },
+  { serial: '03', title: '安洁莉娜', desc: '#信使#', href: 'https://prts.wiki/w/%E5%AE%89%E6%B4%81%E8%8E%89%E5%A8%9C' },
+  { serial: '04', title: '莱茵生命', desc: '#科研#', href: 'https://prts.wiki/w/%E6%B3%B0%E6%8B%89%E5%A4%A7%E5%85%B8:%E7%BB%84%E7%BB%87/%E8%8E%B1%E8%8C%B5%E7%94%9F%E5%91%BD' },
+  { serial: '05', title: '端午', desc: '#炎国水乡#', href: 'https://prts.wiki/w/%E6%B3%B0%E6%8B%89%E5%A4%A7%E5%85%B8:%E5%9C%B0%E7%90%86/%E7%82%8E' },
+  { serial: '06', title: '龙门', desc: '#邻街一角#', href: 'https://prts.wiki/w/%E6%B3%B0%E6%8B%89%E5%A4%A7%E5%85%B8:%E5%9C%B0%E7%90%86/%E9%BE%99%E9%97%A8' },
+  { serial: '07', title: '谜团', desc: '#源石#', href: 'https://prts.wiki/w/%E6%BA%90%E7%9F%B3%E8%99%AB' },
+  { serial: '08', title: '启航', desc: '#任务外出#', href: 'https://ak.hypergryph.com/' },
+  { serial: '09', title: '企鹅物流', desc: '#办公室#', href: 'https://prts.wiki/w/%E6%B3%B0%E6%8B%89%E5%A4%A7%E5%85%B8:%E7%BB%84%E7%BB%87/%E4%BC%81%E9%B9%85%E7%89%A9%E6%B5%81' },
+  { serial: '10', title: '启示', desc: '#大厅#', href: 'https://ak.hypergryph.com/' },
+  { serial: '11', title: '死灰复燃', desc: '#不死的黑蛇#', href: 'https://prts.wiki/w/%22%E4%B8%8D%E6%AD%BB%E7%9A%84%E9%BB%91%E8%9B%87%22' },
+  { serial: '12', title: '苏醒', desc: '#石棺之间#', href: 'https://ak.hypergryph.com/' },
+  { serial: '13', title: '乌萨斯学生自治团', desc: '#回忆中的教室#', href: 'https://prts.wiki/w/%E6%B3%B0%E6%8B%89%E5%A4%A7%E5%85%B8:%E7%BB%84%E7%BB%87/%E4%B9%8C%E8%90%A8%E6%96%AF%E5%AD%A6%E7%94%9F%E8%87%AA%E6%B2%BB%E5%9B%A2' },
+  { serial: '14', title: '瑕光', desc: '#胜利的荣光#', href: 'https://prts.wiki/w/%E7%91%95%E5%85%89' },
+  { serial: '15', title: '谢拉格', desc: '#喀兰贸易#', href: 'https://prts.wiki/w/%E6%B3%B0%E6%8B%89%E5%A4%A7%E5%85%B8:%E5%9C%B0%E7%90%86/%E8%B0%A2%E6%8B%89%E6%A0%BC' },
+  { serial: '16', title: '预备组', desc: '#行动预备组A4#', href: 'https://prts.wiki/w/%E6%96%87%E4%BB%B6:%E5%9B%A2%E9%98%9Flogo_%E8%A1%8C%E5%8A%A8%E9%A2%84%E5%A4%87%E7%BB%84A4.png' },
+  { serial: '17', title: '整装出发', desc: '#印象图#', href: 'https://ak.hypergryph.com/' },
+  { serial: '18', title: '追忆', desc: '#过去的村庄#', href: 'https://ak.hypergryph.com/' },
 ]
 
 for (let i = 0; i < carouselList.length; i++) {
   carouselList[i].thumbnail = `img/carousel_${i + 1}.png`
 }
 
-/**
- * 初始化輪播樣式
- */
+// 初始化輪播樣式
 carouselList.forEach((item, index) => {
   document.querySelector('#media-list').innerHTML += `
   <div 
@@ -66,6 +64,7 @@ const mediaImage = mediaMainPic.querySelector('.media-img')
 const mediaList = document.querySelector('#media-list')
 
 // 初始化輪播資訊
+mediaImage.href = carouselList[0].href
 mediaImage.style = `background-image: url(${carouselList[0].thumbnail}); transform-origin: left top; transform: scale(1)`
 mediaSerial.innerHTML = carouselList[0].serial
 mediaTitle.innerHTML = carouselList[0].title
@@ -145,7 +144,7 @@ function carouselItemSwitching(index, direction) {
     .querySelector(`.media-nav-item:nth-child(${index + 1})`)
     .setAttribute('active', 'true')
 
-  imageZoom(0.25, direction, carouselList[index].thumbnail).then(() => {})
+  imageZoom(0.25, direction, carouselList[index].thumbnail, carouselList[index].href).then(() => {})
   slideInText(mediaSerial, direction, 0.2, 0.4, carouselList[index].serial).then(() => {})
   slideInText(mediaTitle, direction, 0.2, 0.5, carouselList[index].title).then(() => {})
   slideInText(mediaDetail, direction, 0.2, 0.6, carouselList[index].desc).then(() => {})
@@ -190,9 +189,11 @@ async function slideInText(element, direction, duration, delay, newText) {
  * 圖片切換動畫
  * @param {number} duration 持續時間
  * @param {string} direction 方向 (1.'left', 2.'right')
- * @param {string} newUrl 切換後的圖片
+ * @param {string} newImg 切換後的圖片
+ * @param {string} href 点击图片转跳链接
  */
-async function imageZoom(duration, direction, newUrl) {
+async function imageZoom(duration, direction, newImg, href) {
+
   let oldImgTransformOrigin
   let newImgTransformOrigin
   if (direction === 'left') {
@@ -206,7 +207,8 @@ async function imageZoom(duration, direction, newUrl) {
   mediaMainPic.innerHTML += mediaMainPic.innerHTML
   const mediaOldImg = mediaMainPic.querySelector('.media-img:nth-child(1)')
   const mediaNewImg = mediaMainPic.querySelector('.media-img:nth-child(2)')
-  mediaNewImg.style.backgroundImage = `url(${newUrl})`
+  mediaNewImg.href = href
+  mediaNewImg.style.backgroundImage = `url(${newImg})`
 
   mediaOldImg.style.transformOrigin = oldImgTransformOrigin
   mediaOldImg.style.transform = 'scale(1)'
